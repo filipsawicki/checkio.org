bbd8510ae51977770dee7eb59ee2bf02300ad9e1
czarny <filip.sawicki85@gmail.com>
2020-01-06 23:59:02 +0100

    petla while?

---
+++ exc2.py
@@ -0,0 +1,30 @@
+def checkio(data):
+    li = list(data)
+    if len(li) < 10:
+        return False
+    else:
+        for i in li:
+            if i.isdigit() == True:
+
+
+
+
+
+
+
+
+
+
+
+print(checkio('A1213pokl'))
+print(checkio('bAse730onE4'))
+# print(checkio('asasasasasasasaas'))
+# print(checkio('QWERTYqwerty'))
+# print(checkio('123456123456'))
+# print(checkio('QwErTy911poqqqq'))
+
+
+
+
+
+
