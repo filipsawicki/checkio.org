6dd4fb0b1fe4db549149045860b5d2fd14a10e7b
czarny <filip.sawicki85@gmail.com>
2020-01-06 13:47:24 +0100

    Zadanie pierwsze
    In this mission you should check if all elements in the given list are equal.

---
+++ exc1.py
@@ -0,0 +1,20 @@
+#In this mission you should check if all elements in the given list are equal.
+
+def all_the_same(elements):
+    # your code here
+    if not elements:
+        return True
+    return [elements[0]] * len(elements) == elements
+
+
+if __name__ == '__main__':
+    print("Example:")
+    print(all_the_same([1, 1, 1]))
+
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert all_the_same([1, 1, 1]) == True
+    assert all_the_same([1, 2, 1]) == False
+    assert all_the_same(['a', 'a', 'a']) == True
+    assert all_the_same([]) == True
+    assert all_the_same([1]) == True
+    print("Coding complete? Click 'Check' to earn cool rewards!")
