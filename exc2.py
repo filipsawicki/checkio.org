c3003cb7c21151d0b2e7d37cf30f5f1dfd3a0aad
czarny <filip.sawicki85@gmail.com>
2020-01-08 00:24:33 +0100

    done!

--- exc2.py
+++ exc2.py
@@ -1,3 +1,7 @@
+# The password will be considered strong enough if its length is greater than or equal to 10 symbols,
+# it has at least one digit, as well as containing one uppercase letter and one lowercase letter in it.
+
+
 def checkio(data):
     li = list(data)
     a = []
